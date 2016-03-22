69ffa25d48910f7783de6775aca179e73073fe5a
nickyzj <reg.zj@outlook.com>
2016-03-22 15:54:10 +0800

    postgre

--- db_create.py
+++ db_create.py
@@ -7,5 +7,6 @@ db.create_all()
 # insert
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m Well."))
+db.session.add(BlogPost("postgres", "We setup a local postgres database."))
 
 db.session.commit()
