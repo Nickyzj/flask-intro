486eb4d7702dd308f25d3924aaf189453056aed6
nickyzj <reg.zj@outlook.com>
2016-03-22 20:17:41 +0800

    update on mac

--- db_create.py
+++ db_create.py
@@ -8,5 +8,6 @@ db.create_all()
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m Well."))
 db.session.add(BlogPost("postgres", "We setup a local postgres database."))
+db.session.add(BlogPost("mac", "postgres instance on mac."))
 
 db.session.commit()
