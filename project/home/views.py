92158a11f83d86f80dbda490634e715fefc35405
nickyzj <reg.zj@outlook.com>
2016-03-28 15:13:02 +0800

    modified:   project/__init__.py 	modified:   project/home/views.py 	modified:   project/models.py 	modified:   project/users/views.py 	modified:   requirements.txt 	modified:   tests.py

--- project/home/views.py
+++ project/home/views.py
@@ -5,7 +5,7 @@
 from project import app, db
 from project.models import BlogPost
 from flask import flash, redirect, session, url_for, render_template, Blueprint
-from functools import wraps
+from flask.ext.login import login_required
 
 ################
 #### config ####
@@ -15,22 +15,6 @@ home_blueprint = Blueprint(
     'home', __name__,
     template_folder='templates'
 )
-
-##########################
-#### helper functions ####
-##########################
-
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
-
 
 ################
 #### routes ####
